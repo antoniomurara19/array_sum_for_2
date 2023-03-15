77b020ea1ae3cf739389f5501d4c92155733182d
antonio murara zabala <antoniomurar4@gmail.com>
2023-03-15 10:22:02 -0300

    adicionei os métodos pop,push,shift, e unshift

--- logic.js
+++ logic.js
@@ -3,17 +3,24 @@ let res = document.getElementById("res")
 
 
 function inserir(){
-
     let cidade = document.getElementById("cidade").value
-
     array_cidade.push(cidade)
-    res.innerHTML = array_cidade 
-
-    
+    res.innerHTML = array_cidade  
 }
 
 function excluir(){
+    let cidade = document.getElementById("cidade").value
+    array_cidade.pop(cidade)
+    res.innerHTML = array_cidade
+}
+function excluir_final(){
+    let cidade = document.getElementById("cidade").value
     array_cidade.shift(cidade)
+    res.innerHTML = array_cidade
+}
+function inserir_inicio(){
+    let cidade = document.getElementById("cidade").value
+    array_cidade.unshift(cidade)
     res.innerHTML = array_cidade
 }
 
