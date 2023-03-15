bb9787072f82387ecca882859f6380507f4a0580
antonio murara zabala <antoniomurar4@gmail.com>
2023-03-15 08:17:56 -0300

    html,css, e js basico

---
+++ logic.js
@@ -0,0 +1,7 @@
+let cidade = ["Tijucas", "Iitapema", "Moscow",1,19,171293127830123102938149124-12412,"@","!!"]
+let res = document.getElementById("res")
+
+function inserir(){
+    console.log(cidade)
+    res.innerHTML = cidade
+}
